6aeb28c300adc443a3587ddbe5f903e3a3ebaf7a
Stephanel <lui@stephanelep.in>
2013-11-19 21:12:13 +0100

    Modifs diverses

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <SDL/SDL.h>
 
+// Fonctions de conversion radians/degrés
 #define r2d(a) (a * 57.295779513082)
 #define d2r(a) (a * 0.017453292519)
 
@@ -12,91 +13,103 @@ void draw_dots(SDL_Surface *surface, int secondes, int totalSecondes, int x, int
 
 int main ( int argc, char** argv )
 {
-    // initialize SDL video
+    // On initialise la SDL
     if ( SDL_Init( SDL_INIT_VIDEO ) < 0 )
     {
-        printf( "Unable to init SDL: %s\n", SDL_GetError() );
+        printf( "Erreur init SDL: %s\n", SDL_GetError() );
         return 1;
     }
 
-    // make sure SDL cleans up before exit
+    // On s'assure que SDL est quitté correctement à l'arrêt du prog
     atexit(SDL_Quit);
 
-    // create a new window
-    SDL_Surface* screen = SDL_SetVideoMode(640, 480, 16,
-                                           SDL_HWSURFACE|SDL_DOUBLEBUF);
+    // Création d'une fenêtre
+    SDL_Surface* screen = SDL_SetVideoMode(640, 480, 16, SDL_HWSURFACE|SDL_DOUBLEBUF);
     if ( !screen )
     {
-        printf("Unable to set 640x480 video: %s\n", SDL_GetError());
+        printf("Impossible d'utiliser la résolution souhaitée: %s\n", SDL_GetError());
         return 1;
     }
 
-    // load an image
+    // On charge le logo CodeBlocks dans une surface
     SDL_Surface* bmp = SDL_LoadBMP("cb.bmp");
     if (!bmp)
     {
-        printf("Unable to load bitmap: %s\n", SDL_GetError());
+        printf("Erreur chargement BMP: %s\n", SDL_GetError());
         return 1;
     }
 
-    // centre the bitmap on screen
+    // Coordonnées de placement de l'image, au centre de la surface d'affichage.
     SDL_Rect dstrect;
     dstrect.x = (screen->w - bmp->w) / 2;
     dstrect.y = (screen->h - bmp->h) / 2;
 
-    // program main loop
+    // Couleurs des éléments visuels
+    Uint32 couleurRoue = SDL_MapRGB(screen->format, 255, 0, 0);
+    Uint32 couleurCadran = SDL_MapRGB(screen->format, 255, 0, 0);
+    Uint32 couleurRoueOff = SDL_MapRGB(screen->format, 32, 32, 32);
+
+    // Rayons des éléments
+    int rayonPoint = 8;
+    int rayonHorloge = 200;
+
+    // Stockage du temps
+    time_t tempsBrut;
+    struct tm * temps;
+
+    // Boucle principale
     bool done = false;
     while (!done)
     {
-        // message processing loop
+        // Traitement des évènements
         SDL_Event event;
         while (SDL_PollEvent(&event))
         {
-            // check for messages
+            // Message ou pas message ?
             switch (event.type)
             {
                 // exit if the window is closed
-            case SDL_QUIT:
-                done = true;
-                break;
+                case SDL_QUIT:
+                    done = true;
+                    break;
 
                 // check for keypresses
-            case SDL_KEYDOWN:
+                case SDL_KEYDOWN:
                 {
                     // exit if ESCAPE is pressed
                     if (event.key.keysym.sym == SDLK_ESCAPE)
                         done = true;
                     break;
                 }
-            } // end switch
-        } // end of message processing
+            }
+        }
 
-        // DRAWING STARTS HERE
+        // DEBUT DE L'AFFICHAGE
 
-        // clear screen
+        // Vidange de la surface d'affichage
         SDL_FillRect(screen, 0, SDL_MapRGB(screen->format, 0, 0, 0));
-
-        // draw bitmap
+        // On affiche l'image
         SDL_BlitSurface(bmp, 0, screen, &dstrect);
 
-        int rayonPoint = 8;
-        int rayonHorloge = 200;
-
-        time_t tempsBrut;
-        struct tm * temps;
+        // On récupère l'heure et la date
         time(&tempsBrut);
         temps = localtime(&tempsBrut);
         printf("%d\n", temps->tm_sec);
-        draw_dots(screen, 60, 60, (screen->w / 2) - rayonHorloge, (screen->h / 2) - rayonHorloge, rayonHorloge, 8, SDL_MapRGB(screen->format, 32, 32, 32));
-        draw_dots(screen, temps->tm_sec, 60, (screen->w / 2) - rayonHorloge, (screen->h / 2) - rayonHorloge, rayonHorloge, 8, SDL_MapRGB(screen->format, 255, 0, 0));
-        draw_dots(screen, 12, 12, (screen->w / 2) - (rayonHorloge + 22), (screen->h / 2) - (rayonHorloge + 22), (rayonHorloge + 22), 8, SDL_MapRGB(screen->format, 255, 0, 0));
-        // DRAWING ENDS HERE
 
-        // finally, update the screen :)
+        // On affiche la roue en mode "off"
+        draw_dots(screen, 60, 60, (screen->w / 2) - rayonHorloge, (screen->h / 2) - rayonHorloge, rayonHorloge, rayonPoint, couleurRoueOff);
+        // Puis on affiche le cadran autour de la roue
+        draw_dots(screen, 12, 12, (screen->w / 2) - (rayonHorloge + 22), (screen->h / 2) - (rayonHorloge + 22), (rayonHorloge + 22), rayonPoint, couleurCadran);
+        // Et enfin on affiche la roue des secondes
+        draw_dots(screen, temps->tm_sec, 60, (screen->w / 2) - rayonHorloge, (screen->h / 2) - rayonHorloge, rayonHorloge, rayonPoint, couleurRoue);
+
+        // FIN DE L'AFFICHAGE
+
+        // Mise à jour de l'affichage
         SDL_Flip(screen);
-    } // end main loop
+    }
 
-    // free loaded bitmap
+    // On décharge l'image du logo CodeBlocks
     SDL_FreeSurface(bmp);
     return 0;
 }
